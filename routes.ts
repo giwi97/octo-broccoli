62796a0971bb951a82c164b7dadc654a1e610ce3
giwi97 <giwanthawijekoon@gmail.com>
2020-06-10 03:15:14 +0530

    Error fixed

--- routes.ts
+++ routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts'
-import { getProducts } from './controller/products'
+import { getProducts } from './controller/products.ts'
 
 const router = new Router()
 
-router.get('/broco/v1/products', getProducts())
+router.get('/broco/v1/products', getProducts)
 
 export default router
