4cb81633f90e4eeb5d6b9a481cac28010117505f
giwi97 <giwanthawijekoon@gmail.com>
2020-06-10 02:45:07 +0530

    Deleted router from server file

--- server.ts
+++ server.ts
@@ -1,19 +1,15 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+import router from './routes.ts'
 
 const port = 5000
 
 const app = new Application()
 
-const router = new Router()
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-router.get('/broco/v1/products', ({ response }: { response: any }) => {
 
-    
-
-})
 
 console.log(`Server is running on port: ${port}`)
 
