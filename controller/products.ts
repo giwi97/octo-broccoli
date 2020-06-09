00da7a998db504ebc9ad1b00ed6a7e6d1853e2ba
giwi97 <giwanthawijekoon@gmail.com>
2020-06-10 03:01:46 +0530

    Created products file and hardcoreded few products

---
+++ controller/products.ts
@@ -0,0 +1,36 @@
+import { Product } from '../types.ts'
+
+let products: Product[] = [
+    {
+
+        pid: "1",
+        name: "Cucumbers",
+        description: "Fresh cucumbers",
+        uprice: 50.00,
+
+    },
+    {
+
+        pid: "2",
+        name: "Papaya",
+        description: "Fresh papayas",
+        uprice: 60.00,
+
+    },
+    {
+
+        pid: "3",
+        name: "Banana",
+        description: "Fresh bananas",
+        uprice: 40.00,
+
+    },
+    {
+
+        pid: "4",
+        name: "Cucumbers",
+        description: "Fresh cucumbers",
+        uprice: 50.00,
+
+    },
+]
