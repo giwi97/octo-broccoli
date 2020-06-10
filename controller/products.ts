70cf445509fb7003be81a55d4d672baeba4a47f6
giwi97 <giwanthawijekoon@gmail.com>
2020-06-11 01:18:48 +0530

    Retrieving products

--- controller/products.ts
+++ controller/products.ts
@@ -1,10 +1,15 @@
 import { RouterContext } from 'https://deno.land/x/oak/mod.ts'
+import db from '../db/mongodb.ts'
+
+const productsCollection = db.collection('products')
+
 // @desc Get all the products
 // @route GET /broco/api/products
 
-const getProducts = (ctx: RouterContext) => {
+const getProducts = async (ctx: RouterContext) => {
 
-    ctx.response.body = 'Get products'
+    const products = await productsCollection.find();
+    ctx.response.body = products
 
 }
 
