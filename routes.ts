642975c3f65441e6ab482bcf2c7dfc6da33ab863
giwi97 <giwanthawijekoon@gmail.com>
2020-06-12 01:46:26 +0530

    Created POST request for retrieve product details using ID

--- routes.ts
+++ routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts'
-import { getProducts, addProducts } from './controller/products.ts'
+import { getProducts, addProducts, getProductsById } from './controller/products.ts'
 
 const router = new Router()
 
 router
     .get('/broco/v1/products', getProducts)
-    //.get('/broco/v1/products/:id', getSpecificProduct)
+    .get('/broco/v1/products/:id', getProductsById)
     .post('/broco/v1/products', addProducts)
 
 export default router
